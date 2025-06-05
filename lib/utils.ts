d99892d740b3ea80db309451acc346b73527eaf6
Tajali1050 <91536182+Tajali1050@users.noreply.github.com>
2025-06-05 21:45:42 +0530

    Added new UI compoenet

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,12 @@
 import { ReadonlyURLSearchParams } from 'next/navigation';
 
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
 export const baseUrl = process.env.VERCEL_PROJECT_PRODUCTION_URL
   ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
   : 'http://localhost:3000';
